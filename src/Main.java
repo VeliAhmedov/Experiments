949b71b62a60c92da443b924e7dc2e40c09fc2e9
ExternalMirror <ahmedovveli.outlook.com>
2024-12-06 16:41:54 +0400

    nested classes

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,99 @@
 import constructorChain.Source;
 import executionOrder.ExeOrder;
+import innerClass.AnonymousOuterClass;
+import innerClass.BankSecurity;
+import innerClass.OuterClass;
 import modifierExamples.AccessModifier;
 import staticExamples.StExp;
 
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-//        //checking static and non-static
-//        Student.studying();
-//        Student student = new Student();
-//        student.learning();
-//        //this here checking method value connection
-//        student.interesting(student.num);
-//        int result = student.m1();
-//        System.out.println(result);
-//        // for checking use of this on putting connection and using constructor to send value
-//        Teacher teacher = new Teacher(21,"Brian");
-//        teacher.whatIsAge();
-//        //for checking super and this keyword constructor, sending value
-//        Dog dog = new Dog("buddy", "Golden");
-//        dog.Display();
+        //checking static and non-static
+        /*
+        Student.studying();
+        Student student = new Student();
+        student.learning();
+         */
+
+        //this here checking method value connection
+        /*
+        student.interesting(student.num);
+        int result = student.m1();
+        System.out.println(result);
+         */
+
+        // for checking use of this on putting connection and using constructor to send value
+        /*
+        Teacher teacher = new Teacher(21,"Brian");
+        teacher.whatIsAge();
+         */
+
+        // for checking super and this keyword constructor, sending value
+        /*
+        Dog dog = new Dog("buddy", "Golden");
+        dog.Display();
+         */
+
         //experiment on constructor chain via extend
-//        Source s = new Source();
+        /*
+        Source s = new Source();
+         */
 
         //this ere checks how many times object ofc lass created via increment inside no-argument constructor
-//        StExp stExp = new StExp();
-//        stExp.display();
-//        StExp stExp2 = new StExp();
-//        stExp2.display();
-//        StExp stExp3 = new StExp();
-//        stExp3.display();
+        /*
+        StExp stExp = new StExp();
+        stExp.display();
+        StExp stExp2 = new StExp();
+        stExp2.display();
+        StExp stExp3 = new StExp();
+        stExp3.display();
+         */
 
         //adding value via constructor and calling method in car class
-//        Car car = new Car("BMW", 2004);
-//        Car.fueling(car);
+        /*
+        Car car = new Car("BMW", 2004);
+        Car.fueling(car);
+         */
 
         //this is experiment  on execution order as for block (static or not), variables (same), methods (same)
         // constructor (all 3 type) and main method which is static
-//        System.out.println("Main method starts");
-//        ExeOrder exeOrder = new ExeOrder();
-//        ExeOrder exeOrder1 = new ExeOrder(300);
-//        System.out.println("Main method ends");
+        /*
+        System.out.println("Main method starts");
+        ExeOrder exeOrder = new ExeOrder();
+        ExeOrder exeOrder1 = new ExeOrder(300);
+        System.out.println("Main method ends");
+         */
 
         // you can assign values even in final reference.
         // final means that you can assign to new object
-//        final List<Integer> list = new ArrayList<Integer>();
-//        list.add(20);
-//        list.add(30);
-//        list.add(40);
-//        System.out.println("Elements in the list");
-//        System.out.println(list);
+        /*
+        final List<Integer> list = new ArrayList<Integer>();
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println("Elem
+         */
 
         //testing modifiers
-//        AccessModifier accessModifier = new AccessModifier();
-//        accessModifier.access = 1;
+        /*
+        AccessModifier accessModifier = new AccessModifier();
+        accessModifier.access = 1;
+         */
+
+        //creation of inner class object and calling same named method
+        /*
+        OuterClass outer = new OuterClass();
+        OuterClass.InnerClass inner = outer.new InnerClass();
+        outer.display();
+        inner.display();
+         */
+
+        //Anonymous class example. here we call override method inside class
+        /*
+        AnonymousOuterClass a = new AnonymousOuterClass();
+        a.aoco.MethodThatWillBeOverridden();
+         */
 
     }
     }
---
+++ src/innerClass/MethodInnerClass.java
@@ -0,0 +1,35 @@
+package innerClass;
+
+public class MethodInnerClass {
+    int x = 10; // Instance variable of class OuterClass1.
+    static int y = 20; // Static variable of class OuterClass1.
+
+    // Declaration of the static method.
+    static void m3() {
+        System.out.println("I am in OuterClass1 static method");
+    }
+
+    // Declaration of a static method in OuterClass2.
+    public void m1() {
+        //in normal, you can't access local but with final you can. though beyond java 8, it isn't necessary, you should do it
+        //because it will behave like final
+        final int sys = 1;
+// This is a static area of m1(). So we can call only static members.
+// Method local inner class starts here.
+
+        class InnerClass1 {
+            public void m2() {
+                System.out.println(sys);
+                System.out.println("Value of x: " + x); //if method static, not access
+                System.out.println("Value of y: " + y);
+                m3();
+            }
+        }
+        InnerClass1 ic = new InnerClass1();
+        ic.m2();
+    }
+}
+/*
+java doesn't support nested methods but you can use method inner class (which is class that inside of method) to use method inside class inside method
+in method inner class you can't directly access local variables of method you are inside. depending on static (only static) or non-static (both static and non) method you can access instance or static variables and methods
+ */
